375b5b7bf03918e1fb2e07ccdcbe817917afc099
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2021-06-25 20:31:07 +0200

    - create home page with carousel - start working on services page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './carousel/carousel.component';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    HomeComponent,
+    FooterComponent,
+    OurServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    IvyCarouselModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
+export class CarouselComponent implements OnInit {
+  images = [
+    {path: 'http://www.loc-pros.com/images/sliderr1.jpg'},
+    {path: 'http://www.loc-pros.com/images/slider2.jpg'},
+    {path: 'http://www.loc-pros.com/images/slider3.jpg'},
+    {path: 'http://www.loc-pros.com/images/slider4.jpg'},
+]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
