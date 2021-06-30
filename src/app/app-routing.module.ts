df75fcd7e9335be595d79b9afc3a1a5bff3d675c
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2021-06-30 21:17:46 +0200

    remove unnecessary code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
 import { OurServicesComponent } from './our-services/our-services.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'services', component: OurServicesComponent}
+  {path: 'services', component: OurServicesComponent},
+  {path: 'aboutus', component: AboutUsComponent},
+  {path: 'contactUs', component: ContactUsComponent},
+
 ];
 
 @NgModule({
