1b0668cf4e72ce2170d0699811f83786391eed39
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2021-06-30 21:21:48 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'services', component: OurServicesComponent},
   {path: 'aboutus', component: AboutUsComponent},
-  {path: 'contactUs', component: ContactUsComponent},
+  {path: 'contactus', component: ContactUsComponent},
 
 ];
 
